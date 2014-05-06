431d2c284c62b70a570e255e9767ba12f860fc9a
root <root@029d822e-d5ba-ec43-aedc-7cc0e3f5cf02>
2014-05-06 03:48:38 +0000

    初始导入三个php项目 1、PHP:基本演示 2、rewritePhp：演示rewrite功能 3、Zend Framework 2框架程序 4、Apache的httpd.conf、httpd-vhosts.conf, Php的php.ini, 以及rewrite的.htaccess配置备份
    git-svn-id: svn://localhost/ZendStudio@4 029d822e-d5ba-ec43-aedc-7cc0e3f5cf02

---
+++ PHP/index.php
@@ -0,0 +1,7 @@
+<?php
+	header("Content-Type:text/html;charset=utf-8"); 
+	echo "需要将IE中的代理服务器去除，否则将到代理服务器上找域名zgz.com，为何?<br>";
+	echo "项目中conf目录下为Apache、PHP、项目.htaccess等配置的备份<br>";
+	echo "采用UTF-8字符编码<br><br>";
+    phpinfo();
+?>
---
+++ rewritePhp/enter.php
@@ -0,0 +1,6 @@
+<?php
+	header("Content-Type:text/html;charset=utf-8"); 
+    echo "这是唯一的入口<br>";
+    echo "采用了rewrite技术，设计Apache的httpd.conf、httpd-vhosts.conf以及该项目目录下的.htaccess配置文件<br>";
+	echo "采用了虚拟主机<br>"
+?>
